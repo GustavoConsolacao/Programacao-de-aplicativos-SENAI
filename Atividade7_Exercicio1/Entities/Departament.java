881e4a81897f85a3d1021fe24e12a1ba1a73327c
Gustavo Çonsolação <gustavogcsantos66@gmail.com>
2026-06-07 19:38:53 -0300

    Add files via upload

---
+++ Atividade7_Exercicio1/Entities/Departament.java
@@ -0,0 +1,23 @@
+package Atividade7_Exercicio1.Entities;
+
+public class Departament {
+    private String name;
+
+    public Departament(String departmentName) {
+    }
+
+    public void Department() {
+    }
+
+    public void Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Atividade7_Exercicio1/Entities/Enums/WorkerLevel.java
@@ -0,0 +1,7 @@
+package Atividade7_Exercicio1.Entities.Enums;
+
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
